176acf6cf4bf8184b3f9ad394f34ce367a82ba31
McJeffr <jeffrey.lingen@hotmail.com>
2015-11-21 19:08:23 +0100

    Accountbeheerder "Edit Account" interface toegevoegd.

---
+++ src/Fys/Controllers/AccountEditController.java
@@ -0,0 +1,75 @@
+package Fys.Controllers;
+
+import Fys.Models.User;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class. This class controls the Edit Account screen including it's
+ * features.
+ *
+ * @author Jeffrey van der Lingen, IS106-2
+ */
+public class AccountEditController implements Initializable {
+    
+    public static User currentUser;
+    
+    @FXML private Label lblUsername;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        lblUsername.setText(currentUser.getUsername());
+    }
+    
+    @FXML
+    private void btnAccountEvent(ActionEvent event) throws IOException {
+        AccountOverviewController.currentUser = currentUser;
+        ((Node) event.getSource()).getScene().getWindow().hide();
+        Parent parent = FXMLLoader.load(getClass().getResource("/Fys/Views/AccountOverview.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(parent);
+        scene.getStylesheets().add("/Fys/Content/Css/stylesheet.css");
+        stage.setScene(scene);
+        stage.setTitle("Account Overview");
+        stage.show();
+    }
+    
+    @FXML
+    private void btnSaveChangesEvent(ActionEvent event) {
+        System.out.println("Changes Saved");
+    }
+    
+    @FXML
+    private void btnBackToOverviewEvent(ActionEvent event) throws IOException {
+        AccountOverviewController.currentUser = currentUser;
+        ((Node) event.getSource()).getScene().getWindow().hide();
+        Parent parent = FXMLLoader.load(getClass().getResource("/Fys/Views/AccountOverview.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(parent);
+        scene.getStylesheets().add("/Fys/Content/Css/stylesheet.css");
+        stage.setScene(scene);
+        stage.setTitle("Account Overview");
+        stage.show();
+    }
+    
+    @FXML
+    private void boxDisableAccountEvent(ActionEvent event) {
+        System.out.println("Box checked");
+    }
+    
+    @FXML
+    private void btnLogoutEvent(ActionEvent event) {
+        System.out.println("Log out");
+    }
+    
+}
